f254b8f4e890b8f2ea03ee029cd21e69b81a1df4
Ludovic Bonheur <lbonheur@angelsoftwares.ca>
2022-10-19 22:57:28 -0400

    add fullscreen image

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,9 +4,15 @@ import React, { Component }  from 'react';
 class Home extends Component {
   render() {
     return (
-        <div className="section">
-            Home page
+      <div className="section">
+        <img className='bg-image' src="https://images8.alphacoders.com/484/484717.jpg" alt="Italian Trulli"/>
+        <div className='screen-image d-flex flex-column justify-content-end align-items-end pb-4'>
+          <div className='w-25 justify-content-start align-items-end'>
+            <h1>Sylvain Behary</h1>
+            <p>Photographe professionnelle</p>
+          </div>
         </div>
+      </div>
     );
   }
 }
